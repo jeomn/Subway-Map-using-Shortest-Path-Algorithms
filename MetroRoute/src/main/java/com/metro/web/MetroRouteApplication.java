9870f53e5ed890bcf73cf93dd898b46062819dba
Ji-Min Jeong <46081831+jeomn@users.noreply.github.com>
2021-01-30 02:39:05 +0900

    feat: Add whole code

---
+++ MetroRoute/src/main/java/com/metro/web/MetroRouteApplication.java
@@ -0,0 +1,13 @@
+package com.metro.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetroRouteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetroRouteApplication.class, args);
+	}
+
+}
